87bf5bc8076aa9fe6d59727f685fd53c667a13df
Miguel Cortes <miguel.cortes@ias.com.co>
2023-01-03 14:41:04 -0500

    Initial commit

---
+++ test/aws-ms-ts.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsMsTs from '../lib/aws-ms-ts-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-ms-ts-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsMsTs.AwsMsTsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
